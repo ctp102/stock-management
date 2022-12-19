860f05ed1209288e6ff61189dd4501385dc3812c
jh.cho <ctp102>
2022-12-19 20:22:10 +0900

    feat: Redis Lettuce를 활용하여 race condition 문제 해결

---
+++ src/main/java/com/example/stockmanagement/facade/LettuceLockStockFacade.java
@@ -0,0 +1,27 @@
+package com.example.stockmanagement.facade;
+
+import com.example.stockmanagement.repository.RedisLockRepository;
+import com.example.stockmanagement.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    public void decrease(Long key, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(key)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(key, quantity);
+        } finally {
+            redisLockRepository.unlock(key);
+        }
+    }
+
+}
